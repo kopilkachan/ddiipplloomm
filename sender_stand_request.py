db863196f1b0b5586fd120fdfdf7761d0d005cc0
Mehrob <mehaazizov2002@gmail.com>
2024-11-20 09:38:07 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -7,8 +7,6 @@ def new_order(order_body):
                          json=order_body)
 
 response = new_order(data.order_body)
-print(response.status_code)
-print(response.json())
 
 def get_order_info_by_track(track):
     return requests.get(configuration.URL_SERVICE + configuration.ORDER_INFO + str(track))
